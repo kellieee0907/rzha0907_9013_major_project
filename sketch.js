e24765682fbd5a8f4f51628da8df7a9b106593d3
kellieee0907 <rzha0907@uni.sydney.edu.au>
2024-05-31 04:22:23 +1000

    Add Row and Column Animations
    The animation starts by drawing a series of coloured squares in rows and columns, one by one, in a sequential order.

--- sketch.js
+++ sketch.js
@@ -44,10 +44,15 @@ let red = '#A53A32';
 let blue = '#39468C';
 let grey = '#D8D6C7';
 
+let rowStartTime, colStartTime;
+
 // The setup function to initialize the canvas and create rectangle objects
 function setup() {
   let canvasSize = min(windowWidth, windowHeight);  // Set the canvas size to the smaller dimension of the window
   createCanvas(canvasSize, canvasSize);            // Create a square canvas
+
+  rowStartTime = millis(); // Initialize row animation start time
+  colStartTime = millis(); // Initialize column animation start time
 
   // Set big rectangle, Layer them on the canvas from largest to smallest.
   bigRectangles.push(new BigRectangle(0.868, 0.22, 0.066, 0.066, red));
@@ -111,107 +116,106 @@ function draw() {
   drawRow(0, 0, rectWidth, rectHeight,
     [yellow, yellow, grey, blue, yellow, yellow, blue, yellow, yellow, yellow, blue, yellow, grey, yellow, red, yellow, yellow, red,
       grey, grey, red, yellow, yellow, grey, yellow, grey, yellow, yellow, yellow, red, yellow, blue, grey, grey, blue, yellow, yellow, red,
-      yellow, yellow, yellow, blue, grey, red, yellow, yellow]);
+      yellow, yellow, yellow, blue, grey, red, yellow, yellow], rowStartTime);
   drawRow(0, canvasSize * 0.06, rectWidth, rectHeight,
     [yellow, yellow, grey, red, yellow, yellow, blue, yellow, yellow, grey, yellow, red, yellow, yellow, grey, yellow, yellow, blue, yellow,
       yellow, red, yellow, yellow, blue, yellow, yellow, grey, yellow, grey, yellow, yellow, grey, yellow, blue, yellow, blue, yellow,
-      yellow, blue, yellow, yellow, yellow, red, grey, yellow, red]);
+      yellow, blue, yellow, yellow, yellow, red, grey, yellow, red], rowStartTime + 500);
   drawRow(canvasSize * 0.058, canvasSize * 0.165, rectWidth, rectHeight,
     [red, yellow, red, yellow, yellow, grey, yellow, blue, yellow, yellow, grey, yellow, yellow, blue, yellow, red, yellow, blue, yellow,
-      grey, red, grey]);
+      grey, red, grey], rowStartTime + 1000);
   drawRow(0, canvasSize * 0.125, rectWidth, rectHeight,
-    [yellow, blue, yellow]);
+    [yellow, blue, yellow], rowStartTime + 1500);
   drawRow(0, canvasSize * 0.216, rectWidth, rectHeight,
-    [yellow, blue, yellow]);
+    [yellow, blue, yellow], rowStartTime + 2000);
   drawRow(0, canvasSize * 0.295, rectWidth, rectHeight,
     [yellow, grey, yellow, blue, yellow, blue, yellow, yellow, red, grey, blue, yellow, yellow, yellow, grey, yellow, yellow, blue, yellow,
       red, yellow, blue, yellow, red, yellow, blue, yellow, red, yellow, blue, yellow, red, yellow, blue, yellow, yellow, grey, yellow,
-      yellow, red, yellow, yellow, blue, yellow, yellow, red, yellow]);
+      yellow, red, yellow, yellow, blue, yellow, yellow, red, yellow], rowStartTime + 2500);
   drawRow(0, canvasSize * 0.386, rectWidth, rectHeight,
     [yellow, yellow, grey, red, yellow, yellow, blue, yellow, yellow, red, yellow, blue, yellow, yellow, yellow, grey, yellow, grey, yellow,
       blue, yellow, blue, yellow, yellow, grey, yellow, grey, yellow, red, yellow, blue, yellow, blue, yellow, yellow, grey, yellow, yellow,
-      yellow, blue, yellow, yellow, red, yellow, yellow, red, yellow]);
+      yellow, blue, yellow, yellow, red, yellow, yellow, red, yellow], rowStartTime + 3000);
   drawRow(0, canvasSize * 0.325, rectWidth, rectHeight,
-    [yellow, red, yellow]);
+    [yellow, red, yellow], rowStartTime + 3500);
   drawRow(canvasSize * 0.838, canvasSize * 0.24, rectWidth, rectHeight,
-    [blue, yellow, yellow, grey, yellow, red]);
+    [blue, yellow, yellow, grey, yellow, red], rowStartTime + 4000);
 
 
 
   // the first row
-  drawRow(0, canvasSize * -0.51, rectWidth, rectHeight*1.02,
+  drawRow(0, canvasSize * -0.51, rectWidth, rectHeight * 1.02,
     [yellow, blue, yellow, yellow, yellow, yellow, yellow, blue, yellow, grey, yellow, yellow, yellow, yellow, yellow, grey, yellow,
       yellow, yellow, yellow, yellow, yellow, yellow, yellow, grey, yellow, grey, yellow, grey, yellow, yellow, yellow, yellow, yellow,
-      grey, grey, yellow, yellow, yellow, grey, blue, yellow, grey, yellow, blue, grey, yellow, grey]);
+      grey, grey, yellow, yellow, yellow, grey, blue, yellow, grey, yellow, blue, grey, yellow, grey], rowStartTime + 4500);
 
   // the second row
   drawRow(0, canvasSize * -0.38, rectWidth, rectHeight,
     [yellow, red, yellow, yellow, yellow, blue, yellow, grey, grey, red, yellow, grey, yellow, blue, yellow, grey, yellow, yellow,
       yellow, yellow, yellow, grey, grey, blue, yellow, red, grey, grey, yellow, yellow, yellow, yellow, grey, grey, yellow,
-      yellow, yellow, grey, grey, grey, red, yellow, yellow, grey, blue, yellow]);
+      yellow, yellow, grey, grey, grey, red, yellow, yellow, grey, blue, yellow], rowStartTime + 5000);
 
   // the third row
   drawRow(0, canvasSize * -0.2, rectWidth, rectHeight,
     [yellow, red, yellow, blue, yellow, grey, red, yellow, yellow, grey, blue, yellow, yellow, grey, yellow, blue, yellow, yellow, yellow,
       red, yellow, yellow, blue, yellow, yellow, red, yellow, blue, yellow, yellow, red, yellow, yellow, yellow, red, yellow, yellow, yellow,
-      yellow, blue, yellow, blue, yellow, red, yellow, grey, blue, grey, yellow]);
+      yellow, blue, yellow, blue, yellow, red, yellow, grey, blue, grey, yellow], rowStartTime + 5500);
 
   // the fourth row
   drawRow(0, canvasSize * -0.12, rectWidth, rectHeight,
     [yellow, red, grey, blue, yellow, yellow, red, grey, yellow, yellow, grey, blue, yellow, yellow, red, grey, grey, yellow, yellow, red,
       grey, grey, grey, grey, blue, yellow, yellow, grey, yellow, grey, yellow, yellow, red, grey, yellow, grey, yellow, yellow, blue, yellow,
-      yellow, yellow, red, yellow, yellow, grey, red, yellow, red, grey]);
+      yellow, yellow, red, yellow, yellow, grey, red, yellow, red, grey], rowStartTime + 6000);
 
 
   //column
   translate(0, (-canvasSize * 0.56));
 
   let rectNewHeight = canvasSize * 0.022;
-  //first column from the left
+  // first column from the left
   drawColumn(canvasSize * 0.022, 0, rectWidth, rectNewHeight,
-    [yellow, blue, grey, yellow, yellow, blue, yellow, yellow, red, yellow, yellow, blue, yellow, red, yellow, grey, red]);
-  //second column from the left
+    [yellow, blue, grey, yellow, yellow, blue, yellow, yellow, red, yellow, yellow, blue, yellow, red, yellow, grey, red], colStartTime);
+  // second column from the left
   drawColumn(canvasSize * 0.066, 0, rectWidth, rectNewHeight,
-    [red, yellow, grey, yellow, yellow, red, yellow, grey, yellow, grey, yellow, yellow, grey, yellow, yellow, yellow, yellow, blue, yellow, yellow, blue, red, yellow, yellow, grey, blue, yellow, yellow, red, yellow, grey, red, grey, red, yellow, red, grey, yellow, yellow, blue, grey, yellow, grey, red, yellow, yellow]);
-  //third column from the left
+    [red, yellow, grey, yellow, yellow, red, yellow, grey, yellow, grey, yellow, yellow, grey, yellow, yellow, yellow, yellow, blue, yellow, yellow, blue, red, yellow, yellow, grey, blue, yellow, yellow, red, yellow, grey, red, grey, red, yellow, red, grey, yellow, yellow, blue, grey, yellow, grey, red, yellow, yellow], colStartTime + 500);
+  // third column from the left
   drawColumn(canvasSize * 0.132, 0, rectWidth, rectNewHeight,
-    [yellow, yellow, yellow, grey, grey, blue, yellow, yellow, blue, yellow, blue, blue, yellow, red, grey, grey, blue, grey, blue, grey, red, yellow, blue, yellow, yellow, blue, yellow, yellow, blue, yellow, yellow, blue, blue, red, yellow, blue, grey, red, yellow, blue, grey, blue, grey, blue]);
-  //forth column from the left
+    [yellow, yellow, yellow, grey, grey, blue, yellow, yellow, blue, yellow, blue, blue, yellow, red, grey, grey, blue, grey, blue, grey, red, yellow, blue, yellow, yellow, blue, yellow, yellow, blue, yellow, yellow, blue, blue, red, yellow, blue, grey, red, yellow, blue, grey, blue, grey, blue], colStartTime + 1000);
+  // forth column from the left
   drawColumn(canvasSize * 0.242, 0, rectWidth, rectNewHeight,
-    [red, yellow, grey, yellow, yellow, blue, yellow, blue, yellow, grey, yellow, blue, yellow, red, yellow, grey, yellow, blue, grey, yellow, blue, grey, yellow, grey, yellow, yellow, blue, yellow, red, yellow, grey, yellow, yellow, blue, yellow, red, grey, yellow, grey, blue, grey, red, yellow, blue, yellow, yellow]);
+    [red, yellow, grey, yellow, yellow, blue, yellow, blue, yellow, grey, yellow, blue, yellow, red, yellow, grey, yellow, blue, grey, yellow, blue, grey, yellow, grey, yellow, yellow, blue, yellow, red, yellow, grey, yellow, yellow, blue, yellow, red, grey, yellow, grey, blue, grey, red, yellow, blue, yellow, yellow], colStartTime + 1500);
 
-  //first column from the right
+  // first column from the right
   drawColumn(canvasSize * 0.95, 0, rectWidth, rectNewHeight,
-    [red, yellow, red, grey, yellow, red, yellow, grey, blue, grey, yellow, blue, grey, yellow, red, yellow, grey, yellow, yellow, grey, red, yellow, blue, grey, yellow, red, yellow, grey, yellow, grey, yellow, yellow, red, grey, blue, yellow, yellow, red, yellow, red, yellow, grey, yellow, yellow, red, yellow, yellow]);
-  //second column from the right
+    [red, yellow, red, grey, yellow, red, yellow, grey, blue, grey, yellow, blue, grey, yellow, red, yellow, grey, yellow, yellow, grey, red, yellow, blue, grey, yellow, red, yellow, grey, yellow, grey, yellow, yellow, red, grey, blue, yellow, yellow, red, yellow, red, yellow, grey, yellow, yellow, red, yellow, yellow], colStartTime + 2000);
+  // second column from the right
   drawColumn(canvasSize * 0.912, 0, rectWidth, rectNewHeight,
-    [yellow, blue, grey, yellow, grey, blue, blue]);
-  drawColumn(canvasSize * 0.912, canvasSize * 0.179, rectWidth, rectNewHeight*0.98,
-    [yellow, grey, red, red, red, grey, yellow, grey, yellow, grey, yellow, yellow, red]);
+    [yellow, blue, grey, yellow, grey, blue, blue], colStartTime + 2500);
+  drawColumn(canvasSize * 0.912, canvasSize * 0.179, rectWidth, rectNewHeight * 0.98,
+    [yellow, grey, red, red, red, grey, yellow, grey, yellow, grey, yellow, yellow, red], colStartTime + 3000);
   drawColumn(canvasSize * 0.912, canvasSize * 0.64, rectWidth, rectNewHeight,
-    [red, grey, yellow, yellow, grey, grey, blue, yellow, yellow, grey]);
-  //third column
-  drawColumn(canvasSize * 0.868, 0, rectWidth, rectNewHeight*0.96,
-    [red, grey, blue, grey, yellow, red, yellow, grey, yellow, grey, red, red, red, grey, yellow, blue, yellow, red]);
+    [red, grey, yellow, yellow, grey, grey, blue, yellow, yellow, grey], colStartTime + 3500);
+  // third column
+  drawColumn(canvasSize * 0.868, 0, rectWidth, rectNewHeight * 0.96,
+    [red, grey, blue, grey, yellow, red, yellow, grey, yellow, grey, red, red, red, grey, yellow, blue, yellow, red], colStartTime + 4000);
 
-  //fourth column
+  // fourth column
   drawColumn(canvasSize * 0.824, 0, rectWidth, rectNewHeight,
-    [yellow, blue, grey, yellow, yellow, yellow, blue, yellow, blue, yellow, yellow, red, yellow, yellow, grey, blue, yellow, blue, yellow, grey, red, yellow, yellow, blue, yellow, red, yellow, grey, blue, grey, yellow, yellow, red, grey, blue, yellow, yellow, red, grey, blue, yellow, yellow, blue, yellow, red, yellow, grey, yellow, yellow, blue, grey, yellow]);
+    [yellow, blue, grey, yellow, yellow, yellow, blue, yellow, blue, yellow, yellow, red, yellow, yellow, grey, blue, yellow, blue, yellow, grey, red, yellow, yellow, blue, yellow, red, yellow, grey, blue, grey, yellow, yellow, red, grey, blue, yellow, yellow, red, grey, blue, yellow, yellow, blue, yellow, red, yellow, grey, yellow, yellow, blue, grey, yellow], colStartTime + 4500);
 
   // fifth column
   drawColumn(canvasSize * 0.52, 0, rectWidth, rectNewHeight,
-    [yellow, grey, red, yellow, grey, yellow, grey, red, grey, yellow, red, yellow, grey, yellow, grey, yellow, blue])
+    [yellow, grey, red, yellow, grey, yellow, grey, red, grey, yellow, red, yellow, grey, yellow, grey, yellow, blue], colStartTime + 5000);
   drawColumn(canvasSize * 0.52, canvasSize * 0.465, rectWidth, rectNewHeight,
-    [grey, yellow, yellow, blue, yellow, yellow, red, grey, yellow, yellow, grey, blue, yellow, grey, yellow, grey, yellow, yellow, red, grey, yellow, red, grey, yellow, yellow, blue, grey, yellow, yellow])
+    [grey, yellow, yellow, blue, yellow, yellow, red, grey, yellow, yellow, grey, blue, yellow, grey, yellow, grey, yellow, yellow, red, grey, yellow, red, grey, yellow, yellow, blue, grey, yellow, yellow], colStartTime + 5500);
 
-  //sixth column
+  // sixth column
   drawColumn(canvasSize * 0.472, 0, rectWidth, rectNewHeight,
-    [red, yellow, grey, yellow, blue, red, yellow, blue, yellow, red, grey, yellow, yellow, blue, yellow, red, grey, blue, yellow, grey, yellow, red, yellow, yellow, grey, yellow, red, grey, red, grey, yellow, grey, yellow, blue, yellow, yellow, red, blue, grey, yellow, red, grey, yellow]);
+    [red, yellow, grey, yellow, blue, red, yellow, blue, yellow, red, grey, yellow, yellow, blue, yellow, red, grey, blue, yellow, grey, yellow, red, yellow, yellow, grey, yellow, red, grey, red, grey, yellow, grey, yellow, blue, yellow, yellow, red, blue, grey, yellow, red, grey, yellow], colStartTime + 6000);
 
-  //inner column
+  // inner column
   drawColumn(canvasSize * 0.592, canvasSize * 0.465, rectWidth, rectNewHeight,
-    [yellow, yellow, grey, yellow, red, yellow, red]);
-
+    [yellow, yellow, grey, yellow, red, yellow, red], colStartTime + 6500);
 
 
   // Display all the big rectangles by iterating over the array and resizing them
@@ -222,21 +226,31 @@ function draw() {
 
 }
 
-// Function to draw a row of colored squares
-function drawRow(x, y, w, h, colors) {
+// Function to draw a row of colored squares with time-based animation
+function drawRow(x, y, w, h, colors, startTime) {
+  let elapsedTime = millis() - startTime;
   for (let i = 0; i < colors.length; i++) {
-    fill(colors[i]);  // Set the fill color for the square
-    rect(x + i * w, y, w, h);  // Draw the square
+    let delay = i * 100; // 100ms delay between each square
+    if (elapsedTime > delay) {
+      fill(colors[i]);  // Set the fill color for the square
+      rect(x + i * w, y, w, h);  // Draw the square
+    }
   }
 }
 
-// Function to draw a column of colored squares
-function drawColumn(x, y, w, h, colors) {
+
+// Function to draw a column of colored squares with time-based animation
+function drawColumn(x, y, w, h, colors, startTime) {
+  let elapsedTime = millis() - startTime;
   for (let i = 0; i < colors.length; i++) {
-    fill(colors[i]);  // Set the fill color for the square
-    rect(x, y + i * h, w, h);  // Draw the square
+    let delay = i * 100; // 100ms delay between each square
+    if (elapsedTime > delay) {
+      fill(colors[i]);  // Set the fill color for the square
+      rect(x, y + i * h, w, h);  // Draw the square
+    }
   }
 }
+
 
 // Adjust canvas size when window is resized
 function windowResized() {
